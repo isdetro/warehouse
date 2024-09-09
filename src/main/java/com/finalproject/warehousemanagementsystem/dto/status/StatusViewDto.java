b4d0040fc8ccd0d3a78aa4229cea8a80e7040ab0
thisisdetroit <isgender.detroit@gmail.com>
2024-09-09 12:30:44 +0400

    fix

--- src/main/java/com/finalproject/warehousemanagementsystem/dto/status/StatusViewDto.java
+++ src/main/java/com/finalproject/warehousemanagementsystem/dto/status/StatusViewDto.java
@@ -4,13 +4,19 @@ import com.finalproject.warehousemanagementsystem.dto.base.BaseViewDto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class StatusViewDto  {
+public class StatusViewDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     Long id;
     String name;
