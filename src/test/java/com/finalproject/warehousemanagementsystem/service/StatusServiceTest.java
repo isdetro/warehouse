0aaf66f399dc997822a190333cd3032ab493924b
thisisdetroit <isgender.detroit@gmail.com>
2024-09-18 18:22:30 +0400

    fix

---
+++ src/test/java/com/finalproject/warehousemanagementsystem/service/StatusServiceTest.java
@@ -0,0 +1,18 @@
+package com.finalproject.warehousemanagementsystem.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class StatusServiceTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
